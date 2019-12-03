6bca7374839ec139d68504f781ea271da07d861c
Julio Lopes <juliolopes.bfg@gmail.com>
2019-12-03 20:50:36 -0300

    Add: new square

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
 "use strict";
 
-import React from "react";
+import React, { Component } from "react";
 import Title from "./title";
+import Square from "./square";
 
-const App = React.createClass({
-  render: function() {
+class App extends Component {
+  render() {
     return (
-      <div>
-        <Title name="Julio Lopes" lastname="da bicicleta" />
+      <div className="container">
+      {['blue', 'red', 'green'].map((square) => (
+        <Square key={square} color={square} />
+      ))}
       </div>
-    )
+    );
   }
-})
+}
 
 export default App;
