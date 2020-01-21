adf188f3fa6b9ba9c2f4dd818b7cabb5c4d191e4
Julio <juliolopes.bfg@gmail.com>
2020-01-21 20:01:01 -0300

    Add: volta aos estudos

--- src/app.js
+++ src/app.js
@@ -1,16 +1,30 @@
 "use strict";
 
 import React, { Component } from "react";
-import Title from "./title";
-import Square from "./square";
+import Button from './button';
+import Square from './square';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      color: 'green'
+    }
+  }
+
   render() {
     return (
-      <div className="container">
-      {['blue', 'red', 'green'].map((square) => (
-        <Square key={square} color={square} />
-      ))}
+      <div>
+        <Square color={this.state.color} />
+
+        {['red', 'green', 'blue'].map((color) => (
+          <Button 
+            key={color}
+            handleClick={() => this.setState({ color })}>
+            {color}
+          </Button>
+
+        ))}
       </div>
     );
   }
---
+++ src/like-button.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import React from 'react'
+import Button from './button'
+
+const LikeButton = () => (
+    <Button handleClick={() => alert("Curtir")}>
+        Curtir
+    </Button>
+
+)
+
+export default LikeButton
