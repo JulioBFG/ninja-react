92cbc750158e4f84cfe0df3b78734b4d7deda20b
Julio <juliolopes.bfg@gmail.com>
2020-02-06 21:57:55 -0300

    Add: adicionado coisas

--- src/app.js
+++ src/app.js
@@ -3,30 +3,31 @@
 import React, { Component } from "react";
 import Button from './button';
 import Square from './square';
+import Timer from './timer';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      color: 'green'
+      showTimer: true
     }
   }
 
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
   render() {
+    console.log('render');
     return (
       <div>
-        <Square color={this.state.color} />
+        {this.state.showTimer && <Timer />}
 
-        {['red', 'green', 'blue'].map((color) => (
-          <Button 
-            key={color}
-            handleClick={() => this.setState({ color })}>
-            {color}
-          </Button>
-
-        ))}
+        <Button onClick={() => {
+          this.setState({ showTimer: !this.state.showTimer })
+        }}> Show / hide Timer</Button>
       </div>
-    );
+    )
   }
 }
 
---
+++ src/timer.js
@@ -0,0 +1,25 @@
+"use strict"
+
+import React, { Component } from "react";
+
+
+export default class Timer extends Component {
+    constructor() {
+        super()
+        this.state = {
+            time: 0
+        }
+    }
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({
+                time: this.state.time + 1
+            })
+        }, 1000)
+        console.log(this.state.time);
+    }
+
+    render() {
+        return <div>Timer: {this.state.time} </div>
+    }
+}
